a3f19db8665526b35d36207948368863fe745bc2
Hamza <37183538+lepetitpatron@users.noreply.github.com>
2019-09-04 15:31:35 +0200

    Update spring-examen
    Project is afgerond. Unieke antwoorden worden opgeslagen in Redis en vervolgens getoont in een tabel.

--- spring-examen/src/main/java/ap/examen/eightball/controller/EightBallController.java
+++ spring-examen/src/main/java/ap/examen/eightball/controller/EightBallController.java
@@ -4,22 +4,28 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ap.examen.eightball.redis.RedisService;
+
 @Controller
 public class EightBallController
 {
+    @Autowired
+    private RedisService service;
+
     ArrayList<String> questions;
+    ArrayList<String> allAnswers;
     ArrayList<String> answers;
     HashSet<String> chosenAnswers;
 
     public EightBallController()
     {
-        questions = new ArrayList<String>();
         answers = new ArrayList<String>();
         chosenAnswers = new HashSet<String>();
 
@@ -54,9 +60,16 @@ public class EightBallController
     @GetMapping("/questions")
     public String showQuestions(Model model)
     {
-        ArrayList<String> allAnswers = new ArrayList<String>();
+        questions = new ArrayList<String>();
+        allAnswers = new ArrayList<String>();
 
-        chosenAnswers.forEach(a -> allAnswers.add(a));
+        for (String s : this.service.keys("question:*"))
+        {
+            String[] QA = this.service.getKey(s).split("-");
+
+            questions.add(QA[0]);
+            allAnswers.add(QA[1]);
+        }
 
         model.addAttribute("questions", questions);
         model.addAttribute("answers", allAnswers);
@@ -69,21 +82,32 @@ public class EightBallController
     {
         int randomNumber = getRandomNumber();
 
-        // Negeer
+        // Antwoord werd reeds gebruikt. Negeer.
         if (chosenAnswers.contains( answers.get(randomNumber) ))
             return "redirect:questions";
 
-        // Markeer antwoordt als gebruikt & Voeg vraag toe
-        chosenAnswers.add( answers.get(randomNumber) );
-        questions.add(question);
+        if (this.service.exists("questioncount"))
+        {
+            this.service.incr("questioncount");
+        }
+        else
+        {
+            this.service.setKey("questioncount", "1");
+        }
 
+        // Sla de vraag en antwoord op in Redis.
+        this.service.setKey(String.format("question:%s", this.service.getKey("questioncount")), question + "-" + answers.get(randomNumber));
+
+        // Markeer antwoordt als gebruikt.
+        chosenAnswers.add( answers.get(randomNumber) );
+    
         return "redirect:questions";
     }
 
     private static int getRandomNumber()
     {
         final int MIN = 0;
-        final int MAX = 24;
+        final int MAX = 19;
 
 		Random r = new Random();
 		return r.nextInt((MAX - MIN) + 1) + MIN;
