5a1abf35917a3c53abbd76c980e5f17ff02598bc
Hamza <37183538+lepetitpatron@users.noreply.github.com>
2019-09-04 14:52:37 +0200

    spring-examen update
    Het EightBall Systeem werkt volledig in memory (nog geen Redis)

--- spring-examen/src/main/java/ap/examen/eightball/controller/EightBallController.java
+++ spring-examen/src/main/java/ap/examen/eightball/controller/EightBallController.java
@@ -2,20 +2,26 @@ package ap.examen.eightball.controller;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EightBallController
 {
+    ArrayList<String> questions;
     ArrayList<String> answers;
+    HashSet<String> chosenAnswers;
 
     public EightBallController()
     {
+        questions = new ArrayList<String>();
         answers = new ArrayList<String>();
+        chosenAnswers = new HashSet<String>();
 
         answers.add("It is certain.");
         answers.add("It is decidedly so.");
@@ -40,9 +46,46 @@ public class EightBallController
     }
 
     @GetMapping("/")
-    @ResponseBody
     public String index()
     {
-        return answers.toString();
+        return "ask";
     }
+
+    @GetMapping("/questions")
+    public String showQuestions(Model model)
+    {
+        ArrayList<String> allAnswers = new ArrayList<String>();
+
+        chosenAnswers.forEach(a -> allAnswers.add(a));
+
+        model.addAttribute("questions", questions);
+        model.addAttribute("answers", allAnswers);
+
+        return "answers";
+    }
+
+    @PostMapping("/")
+    public String getQuestion(@RequestParam("question") String question)
+    {
+        int randomNumber = getRandomNumber();
+
+        // Negeer
+        if (chosenAnswers.contains( answers.get(randomNumber) ))
+            return "redirect:questions";
+
+        // Markeer antwoordt als gebruikt & Voeg vraag toe
+        chosenAnswers.add( answers.get(randomNumber) );
+        questions.add(question);
+
+        return "redirect:questions";
+    }
+
+    private static int getRandomNumber()
+    {
+        final int MIN = 0;
+        final int MAX = 24;
+
+		Random r = new Random();
+		return r.nextInt((MAX - MIN) + 1) + MIN;
+	}
 }
